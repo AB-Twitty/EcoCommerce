f780a1aea1d1d29c8f41ebdb2214255f0fb04426
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2024-10-23 21:16:56 +0300

    Create Login View

---
+++ Eco.Web/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Eco.Web.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eco.Web.Controllers
+{
+	public class CustomerController : Controller
+	{
+		public IActionResult Login(string returnUrl)
+		{
+			//prepare login model 
+			var model = new LoginModel();
+
+			//check curr customer if registered
+
+			//render view
+			return View(model);
+		}
+
+	}
+}
---
+++ Eco.Web/Models/Customer/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eco.Web.Models.Customer
+{
+	public record LoginModel
+	{
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		public bool RememberMe { get; set; }
+	}
+}
